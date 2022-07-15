58f6f7ac7de46834568360ef9b1300d74b911c5d
Kevin Jin <skndmx@gmail.com>
2022-07-15 01:12:44 -0700

    v0.3.0 added min/max/avg rtt

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import find_packages, setup
-from src import __version__
 setup(
     name='pingnet',
     packages=find_packages(),
-    version = __version__,
     entry_points={
           'console_scripts': ['pingnet=src.pingnet:main',],
     },
