e2fdd5abd0946cf09562b920605cd66baf72d8d3
Kevin Jin <skndmx@gmail.com>
2023-02-22 16:49:18 -0800

    0.4.3

--- src/pingnet.py
+++ src/pingnet.py
@@ -15,7 +15,7 @@ from threading import Thread
 if "darwin" in platform:
      import resource # pylint: disable=import-error
 
-version = "0.4.2"
+version = "0.4.3"
 alive = []                              #Empty list to collect reachable hosts
 alive_avg = []                          #Empty list to collect reachable hosts + RTT
 dead = []                          #Empty list to collect unreachable hosts
